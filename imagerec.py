15aac89aae57a363afd0c18dfebf8206d0059cd5
LorenzoFTSIC <lorenzoferritsic@gmail.com>
2023-04-28 18:59:45 -0400

    adjusted openCV parameters to increase accuracy with test case image, added proper image scaling

--- imagerec.py
+++ imagerec.py
@@ -3,6 +3,8 @@ import pytesseract
 import cv2
 import numpy as np
 
+
+
 # myconfig = r"--psm 6 --oem 3"
 myconfig = r"--psm 8 --oem 3"
 
@@ -10,23 +12,29 @@ myconfig = r"--psm 8 --oem 3"
 
 img = cv2.imread("vellum3.jpg")
 
-# img = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+scale_percent = 150 
+width = int(img.shape[1] * scale_percent / 100)
+height = int(img.shape[0] * scale_percent / 100)
+dim = (width, height)
+
+img = cv2.resize(img, dim, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+
+
+img_processed = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img_processed = cv2.medianBlur(img_processed, 9)
 
 # kernel = np.ones((1, 1), np.uint8)
 # img = cv2.dilate(img, kernel, iterations=1)
 # img = cv2.erode(img, kernel, iterations=1)
 
-img_processed = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+threshold, img_processed = cv2.threshold(img_processed, 140, 255, cv2.THRESH_BINARY )
 
-# img_processed = cv2.Canny(img_processed, 125, 175)
+# img_processed = cv2.Canny(img_processed, 50, 150, apertureSize= 5, L2gradient=True)
 
-threshold, img_processed = cv2.threshold(img_processed, 200, 255, cv2.THRESH_BINARY )
-
-img_processed = cv2.medianBlur(img_processed,3)
 
 height, width = img_processed.shape
 
-boxes = pytesseract.image_to_boxes(img_processed, config=myconfig)
+boxes = pytesseract.image_to_boxes(img_processed, config=myconfig, )
 for box in boxes.splitlines():
     box = box.split(" ")
     print(box)
